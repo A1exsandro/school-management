76cf762adbe892edb10d57576451de19268ea737
A1exsandro <alex.con100trado@gmail.com>
2025-09-06 13:20:46 -0400

    reafactor: chances in admin page

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -118,36 +119,29 @@ const menuItems = [
 
 const Menu = () => {
   return (
-    <div className="mt-4 text-sm pl-2">
-      {
-        menuItems.map(i => (
-          <div key={i.title} className="flex flex-col gap-2">
-            <span className="hidden lg:block text-gray-400 font-light my-4">
-              {i.title}
-            </span>
-            {
-              i.items.map(item => (
-                <Link 
-                  href={item.href} 
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link
+                  href={item.href}
                   key={item.label}
-                  className="flex items-center justify-center lg:justify-start
-                  gap-4 text-gray-500 py-2"
+                  className="flex items-center justify-center lg:justify-start gap-4
+                   text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
                 >
-                  <Image 
-                    src={item.icon} 
-                    alt={item.label}
-                    width={20}
-                    height={20}
-                  />
-                  <span className="hidden lg:block">
-                    {item.label}
-                  </span>
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
                 </Link>
-              ))
+              );
             }
-          </div>
-        ))
-      }
+          })}
+        </div>
+      ))}
     </div>
   )
 }
